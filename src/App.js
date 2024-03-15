a06bc8877311c24fa531d4b4806857594e347a93
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2024-03-15 15:09:21 +0000

    page transition logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import Upload from './uploadSection/Upload';
+import Sidebar from './sidebar/Sidebar';
+import Query from './querySection/Query';
+import { useState } from 'react';
+
 function App() {
+  const [uploadSection, setUploadSection] = useState(true);
+  const [querySection, setQuerySection] = useState(false);
+
   return (
-   <div>
-    <Upload/>
+   <div className={"tw-flex tw-flex-row"}>
+    <Sidebar uploadSection={uploadSection} setUploadSection={setUploadSection} querySection={querySection} setQuerySection={setQuerySection}/>
+    {uploadSection && <Upload/>}
+    {querySection && <Query/>}
    </div>
   );
 }
---
+++ src/querySection/Query.js
@@ -0,0 +1,9 @@
+const Query = () => {
+    return (
+        <div>
+            Hello, I'm the query page lol
+        </div>
+    )
+}
+
+export default Query;
