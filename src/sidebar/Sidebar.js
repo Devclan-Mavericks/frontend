e1bf3e52020843b8cec483907244f6c7f6e82b22
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2024-03-15 15:16:52 +0000

    branding of sidebar lol

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -22,6 +22,8 @@ const Sidebar = (props) => {
                 <div id="uploadTab" style={{width:"100%"}} onClick={openUpload} className={uploadSection ? "tw-my-5 tw-px-10 tw-bg-white tw-bg-opacity-10 tw-border-t-4 tw-border-white tw-p-4" : "tw-my-5 tw-px-10 tw-p-4"}>Upload</div>
                 <div id="queryTab" onClick={openQuery} className={querySection ? "tw-my-5 tw-px-10 tw-bg-white tw-bg-opacity-10 tw-border-t-4 tw-border-white tw-p-4" : "tw-my-5 tw-px-10 tw-p-4"}>Query</div>
             </div>
+
+            <div style={{height:"100dvh"}} className={"tw-text-center tw-align-text-bottom tw-flex tw-flex-col-reverse tw-my-4"}>&copy; 2024 Mavericks - Global Code X Devclan</div>
         </div>
     )
 }
